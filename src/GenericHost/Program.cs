839df8e5f24e28eb1d39c4a5d174b40a4999dccd
monte <greg.jonca@gmail.com>
2019-01-03 21:10:35 +0100

    Create project for GenericHost example

---
+++ src/GenericHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace GenericHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
