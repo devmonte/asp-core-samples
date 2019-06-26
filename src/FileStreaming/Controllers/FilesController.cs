69be0ad2a0e673002de39f16a44fcff14f93a839
devmonte <greg.jonca@gmail.com>
2019-06-26 13:01:54 +0200

    Add example of file streaming

---
+++ src/FileStreaming/Controllers/FilesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileStreaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var fileStream = new FileStream(@".\testFile.aira", FileMode.Open);
+            return new FileStreamResult(fileStream, "application/octet-stream");
+        }
+    }
+}
