6e6b1481e3a06192e3086e02395bea0b92133181
devmonte <greg.jonca@gmail.com>
2019-09-02 16:23:47 +0200

    Scaffold mvc project

--- src/MvcViews/Startup.cs
+++ src/MvcViews/Startup.cs
@@ -1,11 +1,15 @@
-﻿using DependencyInjection.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace DependencyInjection
+namespace MvcViews
 {
     public class Startup
     {
@@ -16,16 +20,10 @@ namespace DependencyInjection
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
-            services.Add(ServiceDescriptor.Transient<ISuperService, SuperService>());
-            
-            services.AddSingleton<ISingletonService, SingletonService>();
-            services.AddScoped<IScopedService, ScopedService>();
-            services.AddTransient<ITransientService, TransientService>();
-
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,13 +35,23 @@ namespace DependencyInjection
             }
             else
             {
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
