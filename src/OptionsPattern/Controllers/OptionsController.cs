56d1e1c1463eac738ccaad7040e3f37dc96511d8
monte <greg.jonca@gmail.com>
2019-09-29 09:41:43 +0200

    Add options pattern example

---
+++ src/OptionsPattern/Controllers/OptionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace OptionsPattern.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OptionsController : ControllerBase
+    {
+        private readonly ILogger<OptionsController> _logger;
+        private readonly IOptionsMonitor<OptionsExample> _optionsMonitor;
+
+        public OptionsController(ILogger<OptionsController> logger, IOptionsMonitor<OptionsExample> optionsMonitor)
+        {
+            _logger = logger;
+            _optionsMonitor = optionsMonitor;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var integer = _optionsMonitor.CurrentValue.SomeInteger;
+            var someString = _optionsMonitor.CurrentValue.SomeString;
+            return Ok(someString);
+        }
+    }
+}
---
+++ src/OptionsPattern/OptionsExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionsPattern
+{
+    public class OptionsExample
+    {
+        public OptionsExample()
+        {
+            SomeInteger = 777;
+            SomeString = "DefaultValue";
+        }
+
+        public int SomeInteger { get; set; }
+        public string SomeString { get; set; }
+    }
+}
