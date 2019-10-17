c0a769b09f2d2c6255b6a932184a8ecaa9cc1692
monte <greg.jonca@gmail.com>
2019-10-17 20:49:01 +0200

    Add routing example

---
+++ src/Routing/Controllers/DefaultApiController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultApiController : ControllerBase
+    {
+        [Route("index")]
+        public string Index()
+        {
+            return "Say hello!";
+        }
+
+
+        [Route("secondIndex")]
+        public string Index2()
+        {
+            return "Say hello again!";
+        }
+
+
+        [Route("/cookie")]
+        [HttpGet(Name = "GetCookie")]
+        public string Index3()
+        {
+            return "No cookie!";
+        }
+
+
+    }
+}
---
+++ src/Routing/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Routing.Models;
+
+namespace Routing.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string FirstTestAction()
+        {
+            return "First action";
+        }
+
+        public string SecondTestAction()
+        {
+            return "Second action";
+        }
+
+        public string ThirdTestAction()
+        {
+            return "Third action";
+        }
+    }
+}
