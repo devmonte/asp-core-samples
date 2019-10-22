adaa72be822c2069f46b137fc2a3f586aa14a007
devmonte <greg.jonca@gmail.com>
2019-10-22 11:19:29 +0200

    Add RazorSyntax example

---
+++ src/RazorSyntax/Controllers/ExampleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorSyntax.Models;
+
+namespace RazorSyntax.Controllers
+{
+    public class ExampleController : Controller
+    {
+        private readonly ILogger<ExampleController> _logger;
+
+        public ExampleController(ILogger<ExampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Student"] = new Student { Age = 25, College = "UTP", Name = "John" };
+            return View();
+        }
+
+        public IActionResult IfStatement()
+        {
+            ViewBag.IsMonday = false;
+            return View();
+        }
+
+        public IActionResult ForEachLoop()
+        {
+
+            var list = new List<Student>
+            {
+                new Student { Age = 25, College = "UTP", Name = "John1" },
+                new Student { Age = 25, College = "WSG", Name = "John2" },
+                new Student { Age = 26, College = "UKW", Name = "John3" },
+                new Student { Age = 28, College = "UMK", Name = "John4" }
+
+            };
+            return View(list);
+        }
+
+    }
+}
---
+++ src/RazorSyntax/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSyntax.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string College { get; set; }
+    }
+}
