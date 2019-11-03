fc4411831d6b8c6a259f03ecbf1f17bebb7cafd1
monte <greg.jonca@gmail.com>
2019-11-03 16:57:41 +0100

    Add RCL example

---
+++ src/ExampleRazorClassLib/Areas/Example/Controllers/RCLController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Areas.Example.Controllers
+{
+    [Area("Example")]
+    public class RCLController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
