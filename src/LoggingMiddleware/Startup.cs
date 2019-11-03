10f262be8a3b5fe774882c2605600452b2e0baeb
monte <greg.jonca@gmail.com>
2019-11-03 16:15:06 +0100

    Update two samples to 3.0

--- src/LoggingMiddleware/Startup.cs
+++ src/LoggingMiddleware/Startup.cs
@@ -26,11 +26,11 @@ namespace LoggingMiddleware
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
