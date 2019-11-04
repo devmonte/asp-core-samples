fbf643172f3a4ac2cdc88cede2e122f22ee26491
devmonte <greg.jonca@gmail.com>
2019-11-04 15:55:32 +0100

    Add integration tests sample

---
+++ src/SUT.Tests/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace SUT.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index2_WhenCalled_ShouldReturnValue()
+        {
+            //Arrange
+            //Act
+
+            //Assert
+
+        }
+    }
+}
