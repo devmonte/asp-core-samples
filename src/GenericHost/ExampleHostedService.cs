d89da4bab4923fda7cca80b5b902145138d96aa8
monte <greg.jonca@gmail.com>
2019-11-20 14:07:37 +0100

    Improve workers sample

---
+++ src/GenericHost/ExampleHostedService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GenericHost
+{
+    public class ExampleHostedService : IHostedService
+    {
+        private readonly ILogger<ExampleHostedService> _logger;
+
+        public ExampleHostedService(ILogger<ExampleHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
